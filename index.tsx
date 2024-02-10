9424c53d562ecc4efb5d995055c79d6238cf8155
TayyabArshad07 <150454391+TayyabArshad07@users.noreply.github.com>
2024-02-10 07:25:03 -0800

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import AboutUs from "../Components/A/AboutUs";
+import TeamSection from "../Components/T/TeamSection";
+import Testimonials from "../Components/T/Testimonials";
+import ClientSlider from "../Components/C/ClientSlider";
+import Layout from "../Components/L/Layout";
+import Head from "next/head";
+import BreadCrumbs from "../Components/B/BreadCrumbs";
+import BackToTop from "../Components/B/BackToTop";
+
+const index = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>About</title>
+      </Head>
+      <BreadCrumbs dynamicText={"About Us"} />
+      <AboutUs />
+      <TeamSection />
+      <BackToTop />
+      <Testimonials />
+      <ClientSlider />
+    </Layout>
+  );
+};
+
+export default index;
